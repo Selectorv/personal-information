76219656a580e5ccb181f7f1c849dfba24b8c933
Selectorv <125983196+Selectorv@users.noreply.github.com>
2023-10-06 12:40:19 +0300

    Initial commit

---
+++ Calculator/Animation/js/questions.js
@@ -0,0 +1,59 @@
+//creating an array and passing the number,questions,options and answers
+let questions = [
+    {
+        numb: 1,
+        question:"what does HTML stand for?",
+        answer:"Hyper Text Markup Language",
+        options:[
+            "Hyper Text Markup Language",
+             "javascript library",
+             "Hyper Text Multi Language",
+             "Hyper Tool Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question:"Who is Bill clinton?",
+        answer:"former president of USA",
+        options:[
+            "Common Style Sheet",
+            "former president of USA ",
+            "Computer Style Sheet",
+            "Cascanding Style Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question:"Who is John legend?",
+        answer:"Musician",
+        options:[
+            "A politician",
+            "Musician",
+            "Doctor",
+            "Engineer"
+        ]
+    },
+    {
+        numb: 4,
+        question:"what is the capital city of japan?",
+        answer:"Tokyo",
+        options:[
+            "Simple Querry  Language",
+            "Style Programming Language",
+            "Tokyo",
+            "Structured Querry Language"
+        ]
+    },
+    {
+        numb: 5,
+        question:"which is the longest river in the world?",
+        answer:"Nile",
+        options:[
+            "eXtensible Markup  Language",
+            "Nile",
+            "eXTra Multi-program Language",
+            "eXamine Multiple  Language"
+        ]
+    },
+
+];
