00938656dfe292d73940cd8296bb4d015d1230cb
Selectorv/MY PORTFOLIO <125983196+Selectorv@users.noreply.github.com>
2024-08-22 10:54:32 +0300

    update

---
+++ CARDS/QUIZ-GAME/js/question2.js
@@ -0,0 +1,60 @@
+let questions = [
+    {
+        numb: 1,
+        question:"What does HTML stand for?",
+        answer:"Hypertext Markup Language",
+        options:[
+            "Hypertext Multiple Language",
+             "Hyper Text Style Language",
+             "Africa",
+             "America"
+
+        ]
+    },
+    {
+        numb: 2,
+        question:"Who was the founder of microsoft company?",
+        answer:"Bill Gates",
+        options:[
+            "Elon Musk",
+            " Steve Jobs",
+            "Mark Zuckerberg",
+            "Bill Gates"
+        ]
+    },
+    {
+        numb: 3,
+        question:"Name the African leader who was anti-apartheid activist?",
+        answer:"Nelson Mandela",
+        options:[
+            "Nelson Mandela",
+            "Muammar Gaddafi",
+            "Jomo Kenyatta",
+            "Olesegun Obasanjo"
+
+        ]
+    },
+    {
+        numb: 4,
+        question:"England is ruled by?",
+        answer:"Monarch",
+        options:[
+            "President",
+            "Monarch",
+            "Prime Minister",
+            "Chief Minister"
+        ]
+    },
+    {
+        numb: 5,
+        question:"Which is the odd one out?",
+        answer:"Yam",
+        options:[
+            "Yam",
+            "Mango",
+            "Orange",
+            "WaterMelon"
+        ]
+    },
+
+];
---
+++ CARDS/QUIZ-GAME/js/questions.js
@@ -0,0 +1,61 @@
+//creating an array and passing the number,questions,options and answers
+let questions = [
+    {
+        numb: 1,
+        question:"What does DNA stand for?",
+        answer:"Deoxyribonucleic Acid",
+        options:[
+             "Detoxificating Nutral Acid",
+             "Deoxyribonucleic Acid",
+             "Dental Number of Animals",
+             "Decabornate Acid"
+
+        ]
+    },
+    {
+        numb: 2,
+        question:"Which is the main gas that makes Earth's atmosphere?",
+        answer:"Nitrogen",
+        options:[
+            "Oxygen",
+            "Air",
+            "Hydrogen peroxide",
+            "Nitrogen"
+        ]
+    },
+    {
+        numb: 3,
+        question:" Which metal is the best conductor of electricity?",
+        answer:"Silver",
+        options:[
+            "Silver",
+            "Gold",
+            "Iron",
+            "Copper"
+
+        ]
+    },
+    {
+        numb: 4,
+        question:"Which is the fastest land animal in the world?",
+        answer:"Cheetah",
+        options:[
+            "Lion",
+            "Elephant",
+            "Cheetah",
+            "Leopard"
+        ]
+    },
+    {
+        numb: 5,
+        question:"What does USB stand for?",
+        answer:"Universal Serial Bus",
+        options:[
+            "Universal State  Bus",
+            "Universal Serial  Bank",
+            "Bank of State",
+            "Universal Serial Bus"
+        ]
+    },
+
+];
